e6916300df1ed36988fa1f40349f30b2f8f33bf0
Guo Yunhe <i@guoyunhe.me>
2023-03-24 23:52:45 +0800

    rename bin

--- src/bin/echarts-image.ts
+++ src/bin/echarts-image.ts
@@ -1,14 +1,17 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import { action } from '..';
+import { outputChartImage } from '..';
 
 const program = new Command('echarts-image');
 
 program
-  .argument('[word]', 'Word to print on console')
-  .option('--repeat <times>', 'Print repeat times, 1 by default', parseInt)
-  .action(action);
+  .argument('<input>', 'ECharts option JSON file path')
+  .argument('<output>', 'Output image file path, support .jpg, .png, webp')
+  .option('--width <value>', 'Width of chart. 400 by default', parseInt)
+  .option('--height <value>', 'Height of chart. 300 by default', parseInt)
+  .option('--dpr <value>', 'Device pixel rate of PNG. 1 by default', parseInt)
+  .action(outputChartImage);
 
 program.helpOption('-h, --help', 'Show full help');
 
